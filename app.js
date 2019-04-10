3dd8ac20f89c1e666ad8bdf52cb938debf505bf1
Tj Hannington <TJ@tjs-macbook-air.home>
2019-04-10 21:17:23 +0100

    new key dependancies installed

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function incorrect() {
-  alert("Hmm that doesn't look right");
-}
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose');
+
+app.get('/', function (req, res) {
+      res.send('Local app has started')
+    })
+
+    app.listen(3000)
+
+    console.log("app has started");
