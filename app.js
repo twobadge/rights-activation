c29afacdccd2852f1b74b6694512de9e38d55ee0
thomas hannington <thomasjackhannington@outlook.com>
2020-04-25 21:38:59 +0100

    fixed dropwdown on show page

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 var express = require('express'),
-  methodOverride = require("method-override"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  mongoose = require('mongoose'),
-  ejs = require('ejs');
-  passport = require("passport"),
-  LocalStrategy = require("passport-local"),
-  User = require("./models/users"),
-  Client = require("./models/clients"),
-  Right = require("./models/rights"),
-  Broadcast = require("./models/broadcast"),
-  Social = require("./models/social") 
+    methodOverride = require('method-override'),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
+    ejs = require('ejs');
+passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/users"),
+    Client = require("./models/clients"),
+    Right = require("./models/rights"),
+    Broadcast = require("./models/broadcast"),
+    Social = require("./models/social")
 
 // set the relevant view engine
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(methodOverride("_method"));
 
 // Passport configuration
 app.use(require("express-session")({
-  secret:"Once agains Rusty wins cutest dog!",
-  resave: false,
-  saveUnitialized: false
+    secret: "Once agains Rusty wins cutest dog!",
+    resave: false,
+    saveUnitialized: false
 }));
 
 app.use(passport.initialize());
@@ -35,12 +35,12 @@ passport.deserializeUser(User.deserializeUser());
 
 //Environment variables
 mongoose.connect('mongodb://localhost:27017/rights_activation', {
-  useNewUrlParser: true
+    useNewUrlParser: true
 });
 
 //Routes
-app.get("/", function (req, res) {
-  res.redirect("/clients");
+app.get("/", function(req, res) {
+    res.redirect("/clients");
 })
 
 
@@ -50,80 +50,80 @@ app.get("/", function (req, res) {
 // =================================s
 
 //Index
-app.get("/clients", function (req, res) {
-  Client.find({}, function (err, clients) {
-    if (err) {
-      console.log("Error");
-    } else {
-      res.render("clients", {clients: clients});
-    }
-  });
+app.get("/clients", function(req, res) {
+    Client.find({}, function(err, clients) {
+        if (err) {
+            console.log("Error");
+        } else {
+            res.render("clients", { clients: clients });
+        }
+    });
 });
 
 //New route
-app.get('/clients/new', function (req, res) {
-  res.render("./clients/new")
+app.get('/clients/new', function(req, res) {
+    res.render("./clients/new")
 })
 
 // Create route
-app.post("/clients", function (req, res) {
-  //create client
-  Client.create(req.body.client, function (err, newClient) {
-    if (err) {
-      res.render("./clients/new");
-    } else {
-      res.redirect("./clients");
-    }
-  });
+app.post("/clients", function(req, res) {
+    //create client
+    Client.create(req.body.client, function(err, newClient) {
+        if (err) {
+            res.render("./clients/new");
+        } else {
+            res.redirect("./clients");
+        }
+    });
 });
 
 //Show route
-app.get("/clients/:id", function (req, res) {
-  Client.findById(req.params.id).populate("rights").populate("broadcasts").populate("socials").exec(function (err, foundClient){
-    if (err) {
-      console.log(err);
-      res.redirect("/clients");
-    } else {
-      // console.log(foundClient);
-      res.render("./clients/show", {client: foundClient});
-    }
-  });
+app.get("/clients/:id", function(req, res) {
+    Client.findById(req.params.id).populate("rights").populate("broadcasts").populate("socials").exec(function(err, foundClient) {
+        if (err) {
+            console.log(err);
+            res.redirect("/clients");
+        } else {
+            // console.log(foundClient);
+            res.render("./clients/show", { client: foundClient });
+        }
+    });
 });
 
 //Edit route
-app.get("/clients/:id/edit", function (req, res) {
-  Client.findById(req.params.id, function (err, foundClient) {
-    if (err) {
-      res.redirect("/clients");
-    } else {
-      res.render("./clients/edit", {
-        client: foundClient
-      });
-    }
-  });
+app.get("/clients/:id/edit", function(req, res) {
+    Client.findById(req.params.id, function(err, foundClient) {
+        if (err) {
+            res.redirect("/clients");
+        } else {
+            res.render("./clients/edit", {
+                client: foundClient
+            });
+        }
+    });
 })
 
 //update route
-app.put("/clients/:id", function (req, res) {
-  Client.findByIdAndUpdate(req.params.id, req.body.client, function (err, updatedClient) {
-    if (err) {
-      console.log("Could not update client");
-      res.redirect("/clients");
-    } else {
-      res.redirect("/clients/" + req.params.id);
-    }
-  });
+app.put("/clients/:id", function(req, res) {
+    Client.findByIdAndUpdate(req.params.id, req.body.client, function(err, updatedClient) {
+        if (err) {
+            console.log("Could not update client");
+            res.redirect("/clients");
+        } else {
+            res.redirect("/clients/" + req.params.id);
+        }
+    });
 });
 
 //Delete route
-app.delete("/clients/:id", function (req, res) {
-  Client.findByIdAndRemove(req.params.id, function (err) {
-    if (err) {
-      res.redirect("/clients");
-    } else {
-      res.redirect("/clients")
-    }
-  })
+app.delete("/clients/:id", function(req, res) {
+    Client.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/clients");
+        } else {
+            res.redirect("/clients")
+        }
+    })
 });
 
 // =================================
@@ -132,64 +132,65 @@ app.delete("/clients/:id", function (req, res) {
 
 //rights index
 
-app.get("/rights", function (req, res) {
-  Right.find({}, function (err, rights) {
-    if (err) {
-      console.log("Error");
-    } else {
-      res.render("rights/rights", {
-        rights: rights
-      });
-    }
-  });
+app.get("/rights", function(req, res) {
+    Right.find({}, function(err, rights) {
+        if (err) {
+            console.log("Error");
+        } else {
+            res.render("rights/rights", {
+                rights: rights
+            });
+        }
+    });
 });
 
 
 // new rights
-app.get("/clients/:id/rights/new", function (req, res) {
-  //find client by id 
-  Client.findById(req.params.id, function (err, client) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("rights/new", {
-        client: client
-      });
-    }
-  });
+app.get("/clients/:id/rights/new", function(req, res) {
+    //find client by id 
+    Client.findById(req.params.id, function(err, client) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("rights/new", {
+                client: client
+            });
+        }
+    });
 });
 
 // post rights
-app.post("/clients/:id/rights", function (req, res) {
-  Client.findById(req.params.id, function (err, client) {
-    if (err) {
-      console.log(err);
-      res.redirect("/clients");
-    } else {
-      Right.create(req.body.right, function (err, right) {
+app.post("/clients/:id/rights", function(req, res) {
+    Client.findById(req.params.id, function(err, client) {
         if (err) {
-          console.log(err);
+            console.log(err);
+            res.redirect("/clients");
         } else {
-          client.rights.push(right);
-          client.save();
-          res.redirect("/clients/" + client._id);
+            Right.create(req.body.right, function(err, right) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    client.rights.push(right);
+                    client.save();
+                    res.redirect("/clients/" + client._id);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
 
 //Edit rights
-app.get("/:right_id/edit", function (req, res) {
-  Right.findById(req.params.rights_id, function (err, foundRight) {
-    if (err) {
-      res.redirect("/clients");
-    } else {
-      res.render("./rights/edit", { 
-        client_id: req.params.id, right: foundRight
-      });
-    }
-  });
+app.get("/:right_id/edit", function(req, res) {
+    Right.findById(req.params.rights_id, function(err, foundRight) {
+        if (err) {
+            res.redirect("/clients");
+        } else {
+            res.render("./rights/edit", {
+                client_id: req.params.id,
+                right: foundRight
+            });
+        }
+    });
 })
 
 
@@ -198,50 +199,50 @@ app.get("/:right_id/edit", function (req, res) {
 // =================================
 
 //Broadcast index
-app.get("/broadcasts", function (req, res) {
-  Broadcast.find({}, function (err, broadcasts) {
-    if (err) {
-      console.log("Error");
-    } else {
-      res.render("broadcasts/broadcasts", {
-        broadcast: broadcasts
-      });
-    }
-  });
+app.get("/broadcasts", function(req, res) {
+    Broadcast.find({}, function(err, broadcasts) {
+        if (err) {
+            console.log("Error");
+        } else {
+            res.render("broadcasts/broadcasts", {
+                broadcast: broadcasts
+            });
+        }
+    });
 });
 
 //new broadcast route
-app.get("/clients/:id/broadcasts/new", function (req, res) {
-  //find client by id 
-  Client.findById(req.params.id, function (err, client) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("broadcasts/new", {
-        client: client
-      });
-    }
-  });
+app.get("/clients/:id/broadcasts/new", function(req, res) {
+    //find client by id 
+    Client.findById(req.params.id, function(err, client) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("broadcasts/new", {
+                client: client
+            });
+        }
+    });
 });
 
 // post broadcasts
-app.post("/clients/:id/broadcasts", function (req, res) {
-  Client.findById(req.params.id, function (err, client) {
-    if (err) {
-      console.log(err);
-      res.redirect("/clients");
-    } else {
-      Broadcast.create(req.body.broadcast, function (err, broadcast) {
+app.post("/clients/:id/broadcasts", function(req, res) {
+    Client.findById(req.params.id, function(err, client) {
         if (err) {
-          console.log(err);
+            console.log(err);
+            res.redirect("/clients");
         } else {
-          client.broadcasts.push(broadcast);
-          client.save();
-          res.redirect("/clients/" + client._id);
+            Broadcast.create(req.body.broadcast, function(err, broadcast) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    client.broadcasts.push(broadcast);
+                    client.save();
+                    res.redirect("/clients/" + client._id);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
 
 
@@ -250,96 +251,96 @@ app.post("/clients/:id/broadcasts", function (req, res) {
 // =================================
 
 //Broadcast index
-app.get("/socials", function (req, res) {
-  Social.find({}, function (err, socials) {
-    if (err) {
-      console.log("Error");
-    } else {
-      res.render("socials/socials", {
-        social: social
-      });
-    }
-  });
+app.get("/socials", function(req, res) {
+    Social.find({}, function(err, socials) {
+        if (err) {
+            console.log("Error");
+        } else {
+            res.render("socials/socials", {
+                social: social
+            });
+        }
+    });
 });
 
 //new social data
-app.get("/clients/:id/socials/new", function (req, res) {
-  //find client by id 
-  Client.findById(req.params.id, function (err, client) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("socials/new", {
-        client: client
-      });
-    }
-  });
+app.get("/clients/:id/socials/new", function(req, res) {
+    //find client by id 
+    Client.findById(req.params.id, function(err, client) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("socials/new", {
+                client: client
+            });
+        }
+    });
 });
 
 // post social data
-app.post("/clients/:id/socials", function (req, res) {
-  Client.findById(req.params.id, function (err, client) {
-    if (err) {
-      console.log(err);
-      res.redirect("/clients");
-    } else {
-      Social.create(req.body.social, function (err, social) {
+app.post("/clients/:id/socials", function(req, res) {
+    Client.findById(req.params.id, function(err, client) {
         if (err) {
-          console.log(err);
+            console.log(err);
+            res.redirect("/clients");
         } else {
-          client.socials.push(social);
-          client.save();
-          res.redirect("/clients/" + client._id);
+            Social.create(req.body.social, function(err, social) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    client.socials.push(social);
+                    client.save();
+                    res.redirect("/clients/" + client._id);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
 
 // =================================
 // Auth routes
 // =================================
-app.get("/register", function(req, res){
-  res.render("users/register");
+app.get("/register", function(req, res) {
+    res.render("users/register");
 });
 
 //Handle signup logic
-app.post("/register", function(req, res){
-  var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err){
-      console.log(err);
-      res.render("users/register");
-    }
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/clients");
+app.post("/register", function(req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.render("users/register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/clients");
+        });
     });
-  });
 });
 
 //Show login form
-app.get("/login", function(req,  res){
-  res.render("users/login")
+app.get("/login", function(req, res) {
+    res.render("users/login")
 });
 
 //Handle login form
 app.post("/login", passport.authenticate("local", {
-  successRedirect: "/clients",
-  failureRedirect: "/login"}), 
-  function(req, res){
-});
+        successRedirect: "/clients",
+        failureRedirect: "/login"
+    }),
+    function(req, res) {});
 
 //logout route
-app.get("/logout", function(req, res){
-  req.logout();
-  res.redirect("/clients");
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/clients");
 });
 
-function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect("/login");
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
 }
 
 
--- appFunctions.js
+++ appFunctions.js
@@ -1,19 +1,19 @@
 // Search function 
 function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById("partner-search");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-      } else {
-          li[i].style.display = "none";
-      }
-  }
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("partner-search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
 
 // Table functions
@@ -22,57 +22,52 @@ function sortTable(n) {
     table = document.getElementById("data-table");
     switching = true;
     //Set the sorting direction to ascending:
-    dir = "asc"; 
+    dir = "asc";
     /*Make a loop that will continue until
     no switching has been done:*/
     while (switching) {
-      //start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
-      for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
-        shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("TD")[n];
-        y = rows[i + 1].getElementsByTagName("TD")[n];
-        /*check if the two rows should switch place,
-        based on the direction, asc or desc:*/
-        if (dir == "asc") {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            //if so, mark as a switch and break the loop:
-            shouldSwitch= true;
-            break;
-          }
-        } else if (dir == "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            //if so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
         }
-      }
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        //Each time a switch is done, increase this count by 1:
-        switchcount ++;      
-      } else {
-        /*If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again.*/
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
-          switching = true;
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
         }
-      }
     }
-  }
-
-  //alert
-function deleteWarning() {
-  alert("Doing this will delete ALL client data and you will not be able to recover it")
-};
+}
